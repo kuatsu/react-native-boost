0078d3b0fca3ff3f0a250b6aed49f7aa08754d58
Maximilian Krause <hello@max-krause.com>
2025-02-23 12:00:05 +0100

    test: added more tests

---
+++ src/optimizers/text/__tests__/fixtures/pressables/code.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+<Text>Hello, world!</Text>;
+<Text onPress={() => {
+  console.log('pressed');
+}}>Hello, world!</Text>;
---
+++ src/optimizers/text/__tests__/fixtures/pressables/output.js
@@ -0,0 +1,6 @@
+import { NativeText as _NativeText } from "react-native/Libraries/Text/TextNativeComponent";
+import { Text } from "react-native";
+<_NativeText>Hello, world!</_NativeText>;
+<Text onPress={() => {
+  console.log('pressed');
+}}>Hello, world!</Text>;
