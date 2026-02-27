a6b107ec952b2329f4571f016dced4398673aa21
Maximilian Krause <max@kuatsu.de>
2026-02-27 19:02:41 +0100

    fix: fix failing tests

---
+++ packages/react-native-boost/src/runtime/__tests__/mocks/react-native.ts
@@ -0,0 +1,10 @@
+export const View = () => 'View';
+export const Text = () => 'Text';
+
+export const Platform = {
+  OS: 'ios',
+};
+
+export const StyleSheet = {
+  flatten: <T>(style: T) => style,
+};
