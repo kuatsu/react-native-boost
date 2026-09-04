c9e50e68501b3560c1d617da9fcb8869c7f3231e
Maximilian Krause <max@kuatsu.de>
2026-09-04 19:22:17 +0200

    perf(native-image): narrow text ancestry checks

--- packages/react-native-boost/src/plugin/optimizers/native-image/__tests__/fixtures/unknown-imported-ancestor/output.js
+++ packages/react-native-boost/src/plugin/optimizers/native-image/__tests__/fixtures/unknown-imported-ancestor/output.js
@@ -1,11 +1,25 @@
+import { NativeImage as _NativeImage } from 'react-native-boost/runtime';
 import { Image } from 'react-native';
 import { ExternalWrapper } from './ExternalWrapper';
+const _imageSource = [
+  {
+    uri: 'unknown.png',
+    width: 16,
+    height: 16,
+  },
+];
 <ExternalWrapper>
-  <Image
-    source={{
-      uri: 'unknown.png',
-      width: 16,
-      height: 16,
-    }}
+  <_NativeImage
+    style={[
+      {
+        width: 16,
+        height: 16,
+      },
+      {
+        overflow: 'hidden',
+      },
+    ]}
+    source={_imageSource}
+    resizeMode="cover"
   />
 </ExternalWrapper>;
